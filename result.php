4b6cdecc4d86e07914ee2a4b9f1ed813a375f84f
Antonio-Ribezzo <ribezzo.antonio@yahoo.it>
2023-05-18 17:19:58 +0200

    Style

--- result.php
+++ result.php
@@ -4,6 +4,12 @@
 ?>
 
 
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang='en'>
     <head>
@@ -20,24 +26,21 @@
         <main class="d-flex justify-content-center align-items-center">
             <div class="container">
                 <h1 class="text-center mb-5">Bad Words</h1>
-                <!-- form che porta a result.php con metodo GET  -->
-                <form action="result.php" method="GET">
-                    <div class="mb-3">
-                        <label for="TextArea" class="form-label">Testo</label>
-                        <textarea class="form-control" id="TextArea" rows="6" placeholder="Inserisci il testo" name="paragrafo"></textarea>
-                    </div>
-                    <div class="mb-3">
-                        <label for="Input1" class="form-label">Parola da censurare</label>
-                        <input class="form-control" id="Input1" placeholder="Digita la parola che vuoi censurare" name="badWord">
-                    </div>
-                    <div class="col-auto">
-                        <button type="submit" class="btn btn-primary mb-3">Invia</button>
-                    </div>
-                </form>
+                <!-- paragrafo e lunghezza -->
+                <div class="container1 rounded p-3 mb-3">
+                    <h2>Il paragrafo senza censura è il seguente:</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit dicta, laborum saepe culpa sed error, nisi nostrum placeat illum numquam a distinctio incidunt ipsa itaque recusandae soluta possimus dolorum consequatur.</p>
+                    <h3 class="text-center">La sua lunghezza è di xxx caratteri</h3>
+                </div>
+
+                <!-- paragrafo censurato -->
+                <div class="container1 rounded p-3">
+                    <h2>Il paragrafo censurato è il seguente:</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit dicta, laborum saepe culpa sed error, nisi nostrum placeat illum numquam a distinctio incidunt ipsa itaque recusandae soluta possimus dolorum consequatur.</p>
+                    <h3 class="text-center">La sua lunghezza è di xxx caratteri</h3>
+                </div>
             </div>
         </main>
-
-
         <!-- script Bootstrap -->
         <script src='https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js' integrity='sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe'   crossorigin='anonymous'></script>
         <!-- my script js -->
@@ -50,6 +53,11 @@
 
             main{
                 height: 100vh;
+            }
+
+            .container1{
+                background-color: rgba(0, 0, 0, 0.3);
+                /* color: white; */
             }
         </style>
     </body>
